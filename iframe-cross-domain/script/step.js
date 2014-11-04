614b337a819845959af7f098eeef6d291b2d189f
yinyong <yanni4night@gmail.com>
2014-11-04 15:09:26 +0800

    I simulated a browser skelon

---
+++ iframe-cross-domain/script/step.js
@@ -0,0 +1,11 @@
+/**
+  * Copyright (C) 2014 yanni4night.com
+  * step.js
+  *
+  * changelog
+  * 2014-11-03[19:23:32]:authorized
+  *
+  * @author yanni4night@gmail.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
