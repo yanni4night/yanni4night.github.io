09107a42b3eeccf1a52e4067e03c27de8a3d1886
yinyong02 <yinyong02@baidu.com>
2017-07-13 13:38:54 +0800

    publish 20170713133853

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+rm -rf ../gh-pages
+
+git clone -b gh-pages git@github.com:yanni4night/yanni4night.github.io.git ../gh-pages
+
+rm -rf ../gh-pages/*
+
+bundle exec jekyll build -d ../gh-pages/
+
+cd ../gh-pages
+
+git add -A
+
+now=`date +%Y%m%d%H%M%S`
+
+git commit -m "publish $now"
+
+git push origin gh-pages
